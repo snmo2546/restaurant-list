abb3a0b9f5316db751d31df0797925bbacfec52c
snmo2546 <snmo2546@gmail.com>
2020-10-26 11:34:21 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -86,6 +86,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
