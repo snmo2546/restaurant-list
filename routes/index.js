de9079fb0c831ee67bcc5f6ab749f4d857164350
snmo2546 <snmo2546@gmail.com>
2020-11-10 16:56:56 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
+const users = require('./modules/users')
 
 // set routes
-router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
+router.use('/users', users)
+router.use('/', home)
 
 // export module
 module.exports = router
